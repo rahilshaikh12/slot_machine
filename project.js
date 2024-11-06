3301db20caa38bd26b1086b173fa30775ef81356
Rahil Shaikh <rahilnabi19@gmail.com>
2024-11-05 20:20:24 -0500

    changes to project

--- project.js
+++ project.js
@@ -9,6 +9,27 @@
 
 const prompt = require("prompt-sync")();
 
+const ROWS = 3;
+const COLS = 3;
+
+// object with keys mapped to different values, left side keys, right side values
+const SYMBOLS_COUNT = {
+    A:2,
+    B:4,
+    C:6,
+    D:8
+};
+
+const SYMBOL_VAlUES={
+    A:5,
+    B:4,
+    C:3,
+    D:2
+};
+
+
+
+
 const deposit = () => {
     while(true) {
         const depositAmount = prompt("Enter a deposit amount: ")
@@ -55,6 +76,45 @@ const getBet = (balance, numberOfLines) => {
 
 };
 
+const spin = () => {
+    const symbols = [];
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)){
+        for(let i = 0; i<count;i++){
+            symbols.push(symbol);
+        }
+    }
+const reels = [];
+    for (let i = 0; i< COLS; i++){
+        reels.push([]);
+        const reelSymbols = [...symbols];
+        for (let j = 0; j < ROWS; j++){
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+            const selectedSymbol = reelSymbols[randomIndex];
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex,1);
+
+
+        }
+    }
+    return reels;
+};
+
+const transpose = (reels) => {
+    const rows = [];
+    for (let i = 0; i < ROWS; i++ ){
+        rows.push([]);
+        for (let j = 0; j < COLS; j++){
+            rows[i].push(reels[j][i])
+        }
+    }
+
+
+};
+
+
+
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
+spin();
+console.log(spin())
